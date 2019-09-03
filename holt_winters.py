54fe503da25fc0661a4c3fb2299afa4a000304ba
ljzology <ljzology@163.com>
2019-09-03 16:32:31 +0800

    Update holt_winters.py

--- holt_winters.py
+++ holt_winters.py
@@ -9,170 +9,6 @@ from numpy import array
 from scipy.optimize import fmin_l_bfgs_b
 
 
-# 自适应一次指数平滑
-# def auto_exp_smoothing(data, alpha, k):
-#     '''
-#     一次指数平滑
-#     :param alpha:  平滑系数
-#     :param s:      数据序列， list
-#     :return:       返回一次指数平滑模型参数， list
-#     '''
-#     def RMSE(params, *args):
-#         Y = args[0]
-#         alpha = params
-#         y = [Y[0]]    # 初始化平滑值
-#         for i in range(len(Y)):
-#             y.append(alpha*Y[i] + (1-alpha)*y[i])
-#         rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y[:], y[:-1])]) / len(Y))
-#         return rmse
-
-#     def linear(x, alpha=None):
-#         Y = x[:].values.tolist()
-#         if(alpha == None):
-#             initial_values = np.array(0.2)
-#             boundaries = [(0.01, 0.4)]
-#             type = 'linear'
-#             parameters = fmin_l_bfgs_b(RMSE, x0=initial_values, args=(Y, type), bounds=boundaries, approx_grad=True)
-#             alpha = parameters[0][0]
-#         a = [Y[0]]
-#         for i in range(1, len(Y)):
-#             a.append(alpha*Y[i] + (1-alpha)*a[i-1])
-
-#         rmse = sqrt(sum([(m-n)**2 for m, n in zip(Y[:], a[:])]) / len(Y))
-#         return a, alpha, rmse
-
-#     data = pd.Series(data)
-#     smooth_data, alpha, rmse = linear(data, alpha=alpha)
-
-#     pre_data = smooth_data[-1]
-#     diff = np.array(smooth_data[:-1] - data.values[:-1])
-#     high = pre_data + k * diff.std()
-#     low = pre_data - k * diff.std()
-#     return high, low, pre_data
-
-
-
-
-# def holt_winters(time_series, period, alpha, beta, gamma, k):
-#     time_series = list(time_series)
-#     if len(time_series) < 3*period:
-#         print("the time series is too short!!")
-#         return
-#
-#     def RMSE(params, *args):
-#         Y = args[0]
-#         m = args[1]
-#         alpha, beta, gamma = params
-#         rmse = 0
-#         a = [sum(Y[0:m]) / float(m)]
-#         b = [(sum(Y[m:2 * m]) - sum(Y[0:m])) / m ** 2]
-#         s = [Y[i] - a[0] for i in range(m)]
-#         y = [a[0] + b[0] + s[0]]
-#
-#         for i in range(len(Y)):
-#             a.append(alpha * (Y[i] - s[i]) + (1 - alpha) * (a[i] + b[i]))
-#             b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
-#             s.append(gamma * (Y[i] - a[i] - b[i]) + (1 - gamma) * s[i])
-#             y.append(a[i + 1] + b[i + 1] + s[i + 1])
-#
-#         rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y, y[:-1])]) / len(Y))
-#         return rmse
-#
-#     def additive(x, m, alpha=None, beta=None, gamma=None):
-#         Y = x[:]
-#         if (alpha==None or beta==None or gamma==None):
-#             initial_values = array([0.3, 0.1, 0.1])
-#             boundaries = [(0, 1), (0, 1), (0, 1)]
-#             parameters = fmin_l_bfgs_b(RMSE, x0=initial_values, args=(Y, m), bounds=boundaries, approx_grad=True)
-#             alpha, beta, gamma = parameters[0]
-#         a = [sum(Y[0:m]) / float(m)]
-#         b = [(sum(Y[m:2 * m]) - sum(Y[0:m])) / m ** 2]
-#         s = [Y[i] - a[0] for i in range(m)]
-#         y = [a[0] + b[0] + s[0]]
-#         rmse = 0
-#
-#         for i in range(1, len(Y)):
-#             a.append(alpha * (Y[i-1] - s[i-1]) + (1-alpha) * (a[i-1] + b[i-1]))
-#             b.append(beta * (a[i] - a[i-1]) + (1 - beta) * b[i-1])
-#             s.append(gamma * (Y[i-1] - a[i-1] - b[i-1]) + (1-gamma) * s[i-1])
-#             y.append(a[i] + b[i] + s[i])
-#
-#         rmse = sqrt(sum([(c - d) ** 2 for c, d in zip(Y[:], y[:])]) / len(Y))
-#         return y, alpha, beta, gamma, rmse
-#
-#     smooth_data, alpha, beta, gamma, rmse = additive(time_series, m=period, alpha=alpha, beta=beta, gamma=gamma)
-#
-#     pre_data = smooth_data[-1]
-#     diff = []
-#     for d1, d2 in zip(smooth_data[:-1], time_series[:-1]):
-#         diff.append(d1-d2)
-#     # diff = np.array(smooth_data[:-1] - time_series[:-1])
-#     diff = np.array(diff)
-#     high = pre_data + k * diff.std()
-#     low = pre_data - k * diff.std()
-#     return high, low, pre_data
-#
-# def holt_winters2(time_series, period, alpha, gamma, k):
-#     time_series = list(time_series)
-#     if len(time_series) < 3*period:
-#         print("the time series is too short!!")
-#         return
-#
-#     def RMSE(params, *args):
-#         Y = args[0]
-#         m = args[1]
-#         alpha, gamma = params
-#         rmse = 0
-#         a = [sum(Y[0:m]) / float(m)]
-#         s = [Y[i] - a[0] for i in range(m)]
-#         y = [a[0] + s[0]]
-#
-#         for i in range(len(Y)):
-#             a.append(alpha * (Y[i] - s[i]) + (1 - alpha) * a[i])
-#             s.append(gamma * (Y[i] - a[i]) + (1 - gamma) * s[i])
-#             y.append(a[i + 1] + s[i + 1])
-#
-#         rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y, y[:-1])]) / len(Y))
-#         return rmse
-#
-#     def additive(x, m, alpha=None, gamma=None):
-#         Y = x[:]
-#         if (alpha==None or gamma==None):
-#             initial_values = array([0.3, 0.1])
-#             boundaries = [(0, 1), (0, 1)]
-#             parameters = fmin_l_bfgs_b(RMSE, x0=initial_values, args=(Y, m), bounds=boundaries, approx_grad=True)
-#             alpha, gamma = parameters[0]
-#         a = [sum(Y[0:m]) / float(m)]
-#         s = [Y[i] - a[0] for i in range(m)]
-#         y = [a[0] + s[0]]
-#         rmse = 0
-#
-# #         for i in range(1, len(Y)):
-# #             a.append(alpha * (Y[i-1] - s[i-1]) + (1-alpha) * (a[i-1] + b[i-1]))
-# #             b.append(beta * (a[i] - a[i-1]) + (1 - beta) * b[i-1])
-# #             s.append(gamma * (Y[i-1] - a[i-1] - b[i-1]) + (1-gamma) * s[i-1])
-# #             y.append(a[i] + b[i] + s[i])
-#         for i in range(len(Y)-1):
-#             a.append(alpha * (Y[i+1] - s[i]) + (1 - alpha) * a[i])
-#             s.append(gamma * (Y[i+1] - a[i]) + (1 - gamma) * s[i])
-#             y.append(a[i + 1] + s[i + 1])
-#
-#         rmse = sqrt(sum([(c - d) ** 2 for c, d in zip(Y[:], y[:-1])]) / len(Y))
-#         return y, alpha, gamma, rmse
-#
-#     smooth_data, alpha, gamma, rmse = additive(time_series, m=period, alpha=alpha, gamma=gamma)
-#
-#     pre_data = smooth_data[-2]
-#     diff = []
-#     for d1, d2 in zip(smooth_data[:-1], time_series[:-1]):
-#         diff.append(d1-d2)
-#     # diff = np.array(smooth_data[:-1] - time_series[:-1])
-#     diff = np.array(diff)
-#     high = pre_data + k * diff.std()
-#     low = pre_data - k * diff.std()
-#     return high, low, pre_data
-
-
 def RMSE(params, *args):
     Y = args[0]
     type = args[1]
@@ -231,10 +67,6 @@ def single_exp(x, fc, alpha=None):
         a.append(alpha * Y[i] + (1 - alpha) * a[i-1])
         y.append(a[i + 1])
     rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y[:-fc], y[:-fc - 1])]) / len(Y[:-fc]))
-    print "alpha = ", alpha
-    print "rmse = ", rmse
-    print Y[-fc:]
-    print y[-fc:]
     return Y[-fc:], alpha, rmse
 
 
@@ -257,8 +89,6 @@ def linear(x, fc, alpha=None, beta=None):
         b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
         y.append(a[i + 1] + b[i + 1])
     rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y[:-fc], y[:-fc - 1])]) / len(Y[:-fc]))
-    print "alpha = ", alpha
-    print "beta = ", beta
     return Y[-fc:], alpha, beta, rmse
 
 
